09dc66fd550ac5c1b48320765223274d403d0df5
Mateus <mmachado.csp@gmail.com>
2018-02-01 17:56:46 -0200

    implements recommended books

--- eureka-client/src/main/java/com/eureka/client/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/eureka/client/EurekaClientApplication.java
@@ -2,9 +2,26 @@ package com.eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableEurekaClient
+@EnableHystrix
 @SpringBootApplication
 public class EurekaClientApplication {
+
+	@RequestMapping(value = "/recommendded")
+	public String readingList() {
+		return "Books List..";
+	}
+	
+	@RequestMapping(value = "/buy")
+	public String buyList() {
+		return "Books Other List";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
